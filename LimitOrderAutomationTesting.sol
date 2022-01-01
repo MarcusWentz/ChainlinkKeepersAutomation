787a61a2dced3062f8fbf740f8591c9504b0f103
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2021-12-31 20:28:26 -0500

    Update LimitOrderAutomationTesting.sol

--- LimitOrderAutomationTesting.sol
+++ LimitOrderAutomationTesting.sol
@@ -1,37 +1,80 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
+contract ERC20TokenContract is ERC20('Chainlink', 'LINK') {}
 
 contract DEXOrderBookDAI {
 
-    function addDAIToPool() public payable{
-        require(msg.value == 2*(10**18), "ERROR");
+    address public immutable Owner;
+
+    constructor() {
+        Owner = msg.sender;
     }
 
-    function swapDAIforETH() public payable{
-        // require(address(this).balance > 0,"No funds in contract yet from Owner.");
-        //require DAI from Contract External
-        // msg.sender.transfer(100);
-        require(msg.value == 1*(10**18), "MSG.VALUE ERROR");
-        payable(msg.sender).transfer(2 ether); 
+    modifier ContractOwnerCheck() {
+        require(msg.sender == Owner, "Only contract owner (deployer) can access this function.");
+        _;
     }
 
-    function getBalance() public view returns (uint) {
+    address public DERC20TokenAddressMatic = 0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1; //LET 1 DERC20 = 1 DAI
+    ERC20TokenContract tokenObject = ERC20TokenContract(DERC20TokenAddressMatic);
+
+    function addDAIToPool() ContractOwnerCheck public {
+        require(tokenObject.balanceOf(address(Owner)) >= 3, "Must have 3*10^-18 LINK for pool creation!");
+        require(tokenObject.allowance(Owner,address(this)) >= 3, "Must allow 3 tokens from your wallet in the ERC20 contract!");
+        tokenObject.transferFrom(Owner, address(this), 3); //NEED TO APPROVE EVERY TIME BEFORE YOU SEND LINK FROM THE ERC20 CONTRACT!
+    }
+
+    function addMATICToPool() ContractOwnerCheck public payable{
+        require(msg.value == 1, "You need to have msg.value as 2 to payout worker biweekly!"); //24 = (12 months * 2 time/month * 1 WEI/time)
+    }
+
+    function removeDAIFromPool() ContractOwnerCheck public {
+         tokenObject.transfer(msg.sender, 3); // 2 LINK from contract to user
+    }
+    
+    function removeMATICFromPool() ContractOwnerCheck public {
+        payable(msg.sender).transfer(1); 
+    }
+
+    function swapMATICforDAI() public payable{
+        require(tokenObject.balanceOf(address(this)) >= 3, "Must have 3*10^-18 DAI!");
+        require(msg.value == 1, "MSG.VALUE ERROR");
+        tokenObject.transfer(msg.sender, 3); // 2 LINK from contract to user
+    }
+
+    function swapDAIforMATIC() public {
+        require(address(this).balance > 0,"No funds in contract yet from Owner.");
+        require(tokenObject.balanceOf(address(Owner)) >= 3, "Must have 3*10^-18 LINK for pool creation!");
+        require(tokenObject.allowance(Owner,address(this)) >= 3, "Must allow 3 tokens from your wallet in the ERC20 contract!");
+        tokenObject.transfer(msg.sender, 3); // 2 LINK from contract to user
+        payable(msg.sender).transfer(1); 
+    }
+
+    function getDAIBalance() public view returns (uint) {
+        return tokenObject.balanceOf(address(this));
+    }
+
+    function getMaticBalance() public view returns (uint) {
         return address(this).balance;
     }
 
 }
 
-// contract LimitOrderETHForDAI is KeeperCompatibleInterface {
-contract LimitOrderETHForDAI {
+// contract LimitOrderMATICForDAI is KeeperCompatibleInterface {
+contract LimitOrderMATICForDAI {
 
     uint public lastTimeStamp;
     address public immutable Owner;
     DEXOrderBookDAI public dexOrderBookDAI;
 
-    // Fallback is called when EtherStore sends Ether to this contract.
-    fallback() external payable {}
+    address public DERC20TokenAddressMatic = 0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1; //LET 1 DERC20 = 1 DAI
+    ERC20TokenContract tokenObject = ERC20TokenContract(DERC20TokenAddressMatic);
+
+    fallback() external payable {} //NEEDED FOR ACCEPTING INCOMING MATIC.
 
     constructor(address _dexOrderBookDAI) {
         Owner = msg.sender;
@@ -48,21 +91,36 @@ contract LimitOrderETHForDAI {
         _;
     }
 
-    function CreateLimitOrder() public payable ContractOwnerCheck {
-        require(msg.value == 1*(10**18), "You need to have msg.value as 2 to payout worker biweekly!"); //24 = (12 months * 2 time/month * 1 WEI/time)
+    function CreateDAILimitOrder() public payable ContractOwnerCheck {
+        require(msg.value == 1, "You need to have msg.value as 2 to payout worker biweekly!"); //24 = (12 months * 2 time/month * 1 WEI/time)
     }
 
-    function swap() public payable ContractOwnerCheck {
-        dexOrderBookDAI.swapDAIforETH{value: 1 ether }();
-        // dexOrderBookDAI.addDAIToPool{value: 1 ether }();
+    // function CreateDAILimitOrder() public payable ContractOwnerCheck {
+    //     require(msg.value == 1, "You need to have msg.value as 2 to payout worker biweekly!"); //24 = (12 months * 2 time/month * 1 WEI/time)
+    // }
+
+    function CancelDAILimitOrder() public ContractOwnerCheck LimitOrderExists{
+        payable(Owner).transfer(1); 
     }
 
-    function CancelLimitOrder() public ContractOwnerCheck LimitOrderExists{
-        payable(Owner).transfer(1*(10**18)); 
+    // function CancelDAILimitOrder() public ContractOwnerCheck LimitOrderExists{
+    //     payable(Owner).transfer(1); 
+    // }
+
+    function SwapMATICforDAI() public payable ContractOwnerCheck {
+        dexOrderBookDAI.swapMATICforDAI{value: 1}();
     }
 
-    function getBalance() public view returns (uint) {
+    function SwapDAIforMATIC() public payable ContractOwnerCheck {
+        dexOrderBookDAI.swapDAIforMATIC();
+    }
+
+    function getMaticBalance() public view returns (uint) {
         return address(this).balance;
+    }
+
+    function getDAIBalance() public view returns (uint) {
+        return tokenObject.balanceOf(address(this));
     }
 
     // // function checkUpkeep(bytes calldata) LimitOrderExists external override returns (bool upkeepNeeded, bytes memory) {
@@ -72,6 +130,6 @@ contract LimitOrderETHForDAI {
     // // }
 
     // function performUpkeep(bytes calldata) external override {
-    //     swapContract.swapDAIforETH();
+    //     swapContract.swapDAIforMATIC();
     // }
 }
