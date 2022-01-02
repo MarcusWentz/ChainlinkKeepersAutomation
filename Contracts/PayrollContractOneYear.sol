3fd6a707d01452cb72a8a1b61b10107b7d3fc229
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-01-02 14:22:57 -0500

    Add files via upload

---
+++ Contracts/PayrollContractOneYear.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
+contract PayrollContractOneYear is KeeperCompatibleInterface {
+
+    uint public lastTimeStamp;
+    address public immutable Owner;
+
+    constructor() {
+      Owner = msg.sender;
+      lastTimeStamp = block.timestamp; //Deployment Epoch timestamp.
+    }
+
+    modifier ContractOwnerCheck() {
+        require(msg.sender == Owner, "Only contract owner (deployer) can access this function.");
+        _;
+    }
+
+    function OwnerAddFundsEveryYear() public payable ContractOwnerCheck {
+        require(msg.value == 2, "You need to have msg.value as 2 to payout worker biweekly!"); //26 = (56 weeks * (1/2) biweekly/weeks * 1 WEI/biweekly)
+    }
+
+    function checkUpkeep(bytes calldata) external override returns (bool upkeepNeeded, bytes memory) {
+        require(address(this).balance > 0,"No funds in contract yet from Owner.");
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > 30; //Block every 30 seconds minimum. Biweekly: 1209600 seconds = (14 days*86400 seconds/day)
+    }
+
+    function performUpkeep(bytes calldata) external override {
+        lastTimeStamp = block.timestamp;
+        payable(0x66C1d8A5ee726b545576A75380391835F8AAA43c).transfer(1); 
+    }
+}
